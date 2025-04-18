5a6e6720bb0446d46800a4e38c22e6e93441e531
Peewi <laursen.lau@gmail.com>
2025-04-18 19:09:28 +0200

    Sleep between video frames, to slow down to real speeds

--- Program.cs
+++ Program.cs
@@ -2,30 +2,60 @@
 using Brackethouse.GB;
 using SDL3;
 
-Console.WriteLine("Laus GB emulator!");
+const int TargetFrameDurationNS = 16740000;
+const int TargetFrameDurationNS60Hz = 16666667;
+const string ProgramName = "Peewi's GB emulator!";
+GB boy = null;
+Console.WriteLine(ProgramName);
 if (!SDL.Init(SDL.InitFlags.Video))
 {
 	SDL.LogError(SDL.LogCategory.System, $"SDL could not initialize: {SDL.GetError()}");
 	return;
 }
-if (!SDL.CreateWindowAndRenderer("Laus GB emulator!", 480, 432, 0, out var window, out var renderer))
+if (!SDL.CreateWindowAndRenderer(ProgramName, 480, 432, 0, out var window, out var renderer))
 {
 	SDL.LogError(SDL.LogCategory.Application, $"Error creating window and rendering: {SDL.GetError()}");
 	return;
 }
 SDL.SetRenderLogicalPresentation(renderer, 160, 144, SDL.RendererLogicalPresentation.Letterbox);
+SDL.SetWindowResizable(window, true);
 
+string gamePath = "";
+if (args.Length > 0)
+{
+	gamePath = args[0];
+}
+if (gamePath == "")
+{
 
-GB boy = new GB(args[0], renderer);
+}
+else
+{
+	boy = new GB(gamePath, renderer);
+	SDL.SetWindowTitle(window, $"{ProgramName} {boy.GameTitle}");
+}
+
 bool running = true;
+ulong sleepCompensation = 0;
 while (running)
 {
+	ulong frameStart = SDL.GetTicksNS();
+	boy?.Step();
 	while (SDL.PollEvent(out var e))
 	{
-		if ((SDL.EventType)e.Type == SDL.EventType.Quit)
+		SDL.EventType t = (SDL.EventType)e.Type;
+		if (t == SDL.EventType.Quit)
 		{
 			running = false;
 		}
 	}
-	boy.Step();
+	ulong frameEnd = SDL.GetTicksNS();
+	ulong frameDuration = frameEnd - frameStart;
+	if (frameDuration < TargetFrameDurationNS)
+	{
+		ulong desiredSleep = TargetFrameDurationNS - frameDuration - sleepCompensation;
+		SDL.DelayNS(desiredSleep);
+		ulong actualSleep = SDL.GetTicksNS() - frameEnd;
+		sleepCompensation = actualSleep - desiredSleep;
+	}
 }
