a731addb2a33e21bdfb1a5c32ed425f563d21152
Peewi <laursen.lau@gmail.com>
2025-08-04 10:48:42 +0200

    Ctrl+O to open file dialog, instead of it being automatic on launch

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Brackethouse.GB;
 using SDL3;
 using System.Runtime.InteropServices;
 
-const int TargetFrameDurationNS = 16740000;
+const int TargetFrameDurationNS = 16744444;
 const int TargetFrameDurationNS60Hz = 16666667;
 bool Running = true;
 const string ProgramName = "Peewi's GB emulator!";
@@ -30,31 +30,10 @@ if (args.Length > 0)
 }
 if (gamePath == "")
 {
-	void cb(nint userdata, nint filelist, int filter)
-	{
-		if (filelist != IntPtr.Zero)
-		{
-			nint strPtr = Marshal.ReadIntPtr(filelist);
-			string? filepath = Marshal.PtrToStringUTF8(strPtr);
-			if (filepath != null)
-			{
-				gamePath = filepath;
-				StartGB(gamePath, renderer);
-			}
-			else
-			{
-				SDL.ShowSimpleMessageBox(SDL.MessageBoxFlags.Error, "No file chosen", "No ROM file chosen. Shutting down.", window);
-				Running = false;
-			}
-		}
-		else
-		{
-			SDL.ShowSimpleMessageBox(SDL.MessageBoxFlags.Error, "Error", SDL.GetError(), window);
-		}
-	}
-	SDL.DialogFileFilter gameBoyFilter = new("Game Boy ROM", "gb;gbc");
-	SDL.DialogFileFilter allFilter = new("all files", "*");
-	SDL.ShowOpenFileDialog(cb, 0, window, [gameBoyFilter, allFilter], 2, null, false);
+	SDL.SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	SDL.RenderDebugText(renderer, 1, 1, $"Press CTRL+O to");
+	SDL.RenderDebugText(renderer, 1, 9, $"open ROM file");
+	SDL.RenderPresent(renderer);
 }
 else
 {
@@ -66,12 +45,17 @@ while (Running)
 {
 	ulong frameStart = SDL.GetTicksNS();
 	boy?.Step();
+	SDL.PumpEvents();
 	while (SDL.PollEvent(out var e))
 	{
 		SDL.EventType t = (SDL.EventType)e.Type;
 		if (t == SDL.EventType.Quit)
 		{
 			Running = false;
+		}
+		if (t == SDL.EventType.KeyDown && e.Key.Key == SDL.Keycode.O && (e.Key.Mod & SDL.Keymod.Ctrl) != 0)
+		{
+			OpenROMFileDialog(window, renderer);
 		}
 	}
 	ulong frameEnd = SDL.GetTicksNS();
@@ -88,9 +72,34 @@ boy?.Save(savePath);
 
 void StartGB(string path, nint renderer)
 {
+	boy?.Save(savePath);
 	string dir = Path.GetDirectoryName(path) ?? string.Empty;
 	string f = (Path.GetFileNameWithoutExtension(path) ?? string.Empty) + ".sav";
 	savePath = Path.Combine(dir, f);
 	boy = new GB(gamePath, savePath, renderer);
 	SDL.SetWindowTitle(window, $"{ProgramName} {boy.GameTitle}");
+}
+void OpenROMCallback(nint userdata, nint filelist, int filter)
+{
+	if (filelist != IntPtr.Zero)
+	{
+		nint strPtr = Marshal.ReadIntPtr(filelist);
+		string? filepath = Marshal.PtrToStringUTF8(strPtr);
+		if (filepath != null)
+		{
+			gamePath = filepath;
+			StartGB(gamePath, renderer);
+		}
+	}
+	else
+	{
+		SDL.ShowSimpleMessageBox(SDL.MessageBoxFlags.Error, "Error", SDL.GetError(), window);
+	}
+}
+
+void OpenROMFileDialog(nint window, nint renderer)
+{
+	SDL.DialogFileFilter gameBoyFilter = new("Game Boy ROM", "gb;gbc");
+	SDL.DialogFileFilter allFilter = new("all files", "*");
+	SDL.ShowOpenFileDialog(OpenROMCallback, 0, window, [gameBoyFilter, allFilter], 2, null, false);
 }
